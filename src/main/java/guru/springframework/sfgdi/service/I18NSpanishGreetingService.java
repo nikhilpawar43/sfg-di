edfcdfe912c90a56b884094dd81e827a15e645b1
nikhilpawar43 <nikhilpawar43@ymail.com>
2022-06-04 17:44:53 +0530

    Default profile

--- src/main/java/guru/springframework/sfgdi/service/I18NSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/service/I18NSpanishGreetingService.java
@@ -3,7 +3,7 @@ package guru.springframework.sfgdi.service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18NGreetingService")
 public class I18NSpanishGreetingService implements GreetingService {
     
